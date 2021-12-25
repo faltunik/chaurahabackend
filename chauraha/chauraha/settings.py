e871bb12122d1b01d0d69c52620a3e58735270f0
Nik <sanatnik21@gmail.com>
2021-12-25 20:29:47 +0530

    allowed origini all

--- chauraha/chauraha/settings.py
+++ chauraha/chauraha/settings.py
@@ -188,11 +188,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # later you can change
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://echaura.netlify.app"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://echaura.netlify.app"
+# ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
